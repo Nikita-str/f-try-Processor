b6b35b2d026a2bcf996d75f6ca28b3b5391ebd99
Nikita-str <42584606+Nikita-str@users.noreply.github.com>
2020-10-29 23:49:22 +0300

    Delete generic_function.h

--- Processor/generic_function.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-#include <math.h>
-const double GENERIC_EPS = 1e-6;
-
-inline _Bool DoubleEqualDouble(double d1, double d2) { return fabs(d1 - d2) < GENERIC_EPS; }
-inline _Bool DoubleEqualZero(double d) { return DoubleEqualDouble(d, 0); }
-inline _Bool DoubleAboveZero(double d) { return (d - GENERIC_EPS) > 0; }
