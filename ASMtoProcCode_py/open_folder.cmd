89188e3888ed54d0d70d534106de6aa1c7fca4ba
Nikita-str <42584606+Nikita-str@users.noreply.github.com>
2020-11-02 02:06:25 +0300

    upd tests(and maybe smth add, i dont remember)

--- ASMtoProcCode_py/open_folder.cmd
+++ ASMtoProcCode_py/open_folder.cmd
@@ -1,4 +1,5 @@
 python3.8 asm_to_proc_code.py tests\test_1.txt pcodes\1.#pcode#
 python3.8 asm_to_proc_code.py tests\test_2.txt pcodes\2.#pcode#
-
+python3.8 asm_to_proc_code.py tests\test_3.txt pcodes\3.#pcode#
+python3.8 asm_to_proc_code.py tests\test_4.txt pcodes\4.#pcode#
 cmd
